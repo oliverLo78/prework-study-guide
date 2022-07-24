02afa14f49f19644e2908d201bb232d0c7b8dd21
oliverLo78 <oliverberto@gmail.com>
2022-07-24 14:48:38 -0400

    learned how to create an array of strings and for loop to output content of array

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for(var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+
+/*
+if(topic === 'HTML') {
+    console.log("Lets study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Lets study CSS!");
+} else if (topic === 'Git') {
+    console.log("Lets study Git!");
+} else if (topic === 'JavaScript'){
+    console.log("Lets study JavaScript!");
+} else {
+    console.log('Please try again');
+}
+
+*/
